2dfac464f0002f8217fce712d45e338e85331c70
Shrouk Muhammed <99049615+Shrouk-Saleh@users.noreply.github.com>
2026-03-29 09:25:15 +0200

    .

--- shrouk_12259_project/shrouk_12259_project/Student.cpp
+++ shrouk_12259_project/shrouk_12259_project/Student.cpp
@@ -4,32 +4,40 @@
 using namespace std;
 
 Student::Student(int id, string name, double gpa)
-    : Person(name), id(id), gpa(gpa) {
+    : Person(name) {
+    this->id = id;
+    this->gpa = gpa;
 }
 
-int Student::getId() const {
+int Student::getId() {
     return id;
 }
 
-double Student::getGpa() const {
+double Student::getGpa() {
     return gpa;
 }
 
-bool Student::enrollCourse(const string& course) {
-    return courses.insert(course).second;
+bool Student::enrollCourse(string course) {
+    if (courses.count(course) == 0) {
+        courses.insert(course);
+        return true;
+    }
+    return false;
 }
 
-void Student::showCourses() const {
+void Student::showCourses() {
     if (courses.empty()) {
-        cout << "  No courses enrolled.\n";
+        cout << "No courses.\n";
         return;
     }
-    for (const string& c : courses)
-        cout << "  - " << c << "\n";
+
+    for (string c : courses) {
+        cout << "- " << c << endl;
+    }
 }
 
-void Student::display() const {
+void Student::display() {
     cout << "ID: " << id
-        << "  Name: " << name
-        << "  GPA: " << fixed << setprecision(1) << gpa << "\n";
+         << " Name: " << name
+         << " GPA: " << fixed << setprecision(1) << gpa << endl;
 }
